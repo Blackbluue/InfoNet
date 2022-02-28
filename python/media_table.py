47eedeef6660273497641355cd3a7fcec2758ccb
Raymone Miller <miller_raymone@hotmail.com>
2022-02-28 15:54:04 -0500

    Update and rename media.py to media_table.py
    - fixed outdated import paths.
    - separate table definitions into new modules.
    -rename module to 'media_table' for clarity and consistency.

--- python/media_table.py
+++ python/media_table.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 """media module."""
 from sqlalchemy import Column, Integer, String, Date, Text
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 
 Base = declarative_base()
 
 
+class Media(Base):
+    """Base media table."""
+
+    __tablename__ = "Media"
+    id = Column(Integer, primary_key=True)
+    title = Column(String(32), nullable=False)
+    creator = Column(String(32))
+    release_date = Column(Date)
+    description = Column(Text)
+
+    def __init__(self, title, creator=None, release_date=None, description=None):
+        """Create a record."""
+        self.title = title
+        self.creator = creator
+        self.release_date = release_date
+        self.description = description
+        
 class Anime(Base):
     """Anime table."""
 
