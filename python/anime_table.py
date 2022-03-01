fb73804c4d3c0c6810ba87ad91b142663f9a1191
Raymone Miller <miller_raymone@hotmail.com>
2022-03-01 15:59:22 -0500

    Update anime_table.py

--- python/anime_table.py
+++ python/anime_table.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""media module."""
+"""anime_table module."""
 from sqlalchemy import Column, Integer, String, Date, Text
 from media import Media
 
@@ -7,7 +7,11 @@ class Anime(Media):
     """Anime table."""
 
     __tablename__ = "Anime"
+    episodes = Column(Integer)
+    end_date = Column(Date)
 
-    def __init__(self):
+    def __init__(self, episodes=0, end_date=None):
         """Create a record."""
+        self.episodes = episodes
+        self.end_date = end_date
         
