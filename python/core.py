6654c122e1ebf9f20e477afcc377ebdee93019a0
Raymone Miller <miller_raymone@hotmail.com>
2022-03-02 13:29:11 -0500

    Update core.py

--- python/core.py
+++ python/core.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 """core module."""
 from sqlalchemy import create_engine
-from sqlalchemy import MetaData
+from sqlalchemy.orm import Session
+from media_table import Base
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
-meta = MetaData()
+
+def add_row():
+    with Session(engine) as session:
+        # code to add a row to table
+        session.commit()
+def remove_row():
+    pass
+def change_row():
+    pass
+def empty_table():
+    pass
+
